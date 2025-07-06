ffbe3a6e7b0988e5f63aafbb60451db8f96fd7c3
qKuafn <qkuafn@gmail.com>
2025-07-06 14:19:24 +0900

    workflows修正 6

---
+++ Update_DownloadUrl.py
@@ -0,0 +1,25 @@
+import requests
+import sys
+
+password = sys.argv[1]
+sha = sys.argv[2]
+version = sys.argv[3]
+
+url = "https://fljpapi2-sjnq.onrender.com/qa/upload"
+headers = {
+    "Authorization": password
+}
+data = {
+    "changelogUrl": "https://github.com/Fortniteleakjp/FModel-JP-/releases/tag/qa",
+    "downloadUrl": f"https://github.com/Fortniteleakjp/FModel-JP-/releases/download/qa/{sha}.zip",
+    "version": version
+}
+
+# 認証（パスワード方式）
+response = requests.patch(url, headers=headers, json=data)
+
+# 結果出力
+if response.status_code == 200:
+    print("URL更新完了")
+else:
+    print(f"URL更新失敗 : {response.status_code} - {response.text}")
