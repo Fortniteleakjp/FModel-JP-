58f5ff93563cabe834a70fea5731fe63911a5626
FLJPNews <fljpburi@gmail.com>
2025-07-30 13:29:51 +0900

    エラー修正

--- FModel/FModel/Framework/RelayCommand.cs
+++ FModel/FModel/Framework/RelayCommand.cs
@@ -1,20 +1,27 @@
 using System;
 using System.Windows.Input;
-using System.Collections.Generic;
-using System.Diagnostics;
 
-public class RelayCommand : ICommand
+namespace FModel.Commands
 {
-    private readonly Action _execute;
-    private readonly Func<bool>? _canExecute;
-
-    public RelayCommand(Action execute, Func<bool>? canExecute = null)
+    public class RelayCommand : ICommand
     {
-        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-        _canExecute = canExecute;
-    }
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?>? _canExecute;
 
-    public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
-    public void Execute(object? parameter) => _execute();
-    public event EventHandler? CanExecuteChanged;
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) => _execute(parameter);
+    }
 }
