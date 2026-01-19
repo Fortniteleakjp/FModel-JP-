98cf25acd3590cbf28b7e157c49dbc5af7656517
FLJPNews <fljpburi@gmail.com>
2026-01-20 00:38:04 +0900

    NEW エクスプローラーでスクロールを有効(不具合の修正)

--- Update_DownloadUrl.py
+++ Update_DownloadUrl.py
@@ -4,7 +4,7 @@ import secrets
 
 sha = sys.argv[1]
 # version = sys.argv[2]
-version = f"4.4.5.0.{sha}"
+version = f"4.4.5.1.{sha}"
 
 url = "https://fljpapi2-sjnq.onrender.com/qa/upload"
 headers = {
